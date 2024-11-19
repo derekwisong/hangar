3b7330366ad5b21005f437c9401a53be671053f7
Derek Wisong <derekwisong@gmail.com>
2024-11-18 20:05:12 -0500

    Brought EIS test file into repo

--- benches/data.rs
+++ benches/data.rs
@@ -7,10 +7,6 @@ pub fn read_csv_eager(c: &mut Criterion) {
     c.bench_function("read_csv", |b| b.iter(|| hangar::read_csv(&p).unwrap()));
 }
 
-pub fn read_csv_lazy(c: &mut Criterion) {
-    let p = std::path::Path::new(SAMPLE_CSV);
-    c.bench_function("read_csv_lazy", |b| b.iter(|| hangar::read_lazy(&p).unwrap()));
-}
 
-criterion_group!(benches, read_csv_eager, read_csv_lazy);
+criterion_group!(benches, read_csv_eager);
 criterion_main!(benches);
--- tests/data.rs
+++ tests/data.rs
@@ -1,20 +1,28 @@
 // A Garmin EIS file for a Mooney M20J
-const SAMPLE_CSV: &str = "/mnt/d/Flying/Flight Logs/log_231104_084813_KPOU.csv";
+const SAMPLE_CSV: &str = "log_231104_084813_KPOU.csv";
+
+fn resource_path(filename: &str) -> std::path::PathBuf {
+    let mut d = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("resources");
+    d.push(filename);
+    d
+}
+
 
 #[test]
 fn test_read_csv_metadata() {
-    let path = std::path::Path::new(SAMPLE_CSV);
+    let path = resource_path(SAMPLE_CSV);
     let header = hangar::EISHeader::from_csv(&path).unwrap();
 
     assert_eq!(header.metadata["log_version"], "1.03");
     assert_eq!(header.metadata["airframe_name"], "Mooney M20J");
-    assert_eq!(header.metadata["tail_number"], "N355RL");
+    assert_eq!(header.metadata["tail_number"], "N12345");
 }
 
 // test reading raw column names from a Garmin EIS csv file
 #[test]
 fn read_csv_raw_column_names() {
-    let path = std::path::Path::new(SAMPLE_CSV);
+    let path = resource_path(SAMPLE_CSV);
     let header = hangar::EISHeader::from_csv(&path).unwrap();
     let raw_columns = header.columns.iter().map(|c| c.raw_name()).collect::<Vec<&str>>();
     assert_eq!(
@@ -99,7 +107,7 @@ fn read_csv_raw_column_names() {
 // test reading the column names from a Garmin EIS csv file
 #[test]
 fn read_csv_column_names() {
-    let path = std::path::Path::new(SAMPLE_CSV);
+    let path = resource_path("log_231104_084813_KPOU.csv");
     let header = hangar::EISHeader::from_csv(&path).unwrap();
     let columns = header.columns.iter().map(|c| c.name()).collect::<Vec<&str>>();
     assert_eq!(
